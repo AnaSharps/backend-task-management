fbabb7fc239e24fd02d8fc004eb32a15fba47880
Prashansa Dadu <65507065+AnaSharps@users.noreply.github.com>
2021-07-13 15:40:23 +0530

    Fix unique values for email in db

--- database/migrations/2021_07_13_100609_create_users_table.php
+++ database/migrations/2021_07_13_100609_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('Name');
-            $table->string('Email');
+            $table->string('Email')->unique();
             $table->string('Password');
             $table->string('Role');
             $table->string('Created_by');

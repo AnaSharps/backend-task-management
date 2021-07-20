3c618cefc1581ef41aca87d30e43760219d2ccf3
Prashansa Dadu <65507065+AnaSharps@users.noreply.github.com>
2021-07-20 18:32:58 +0530

    Refactor Code

--- frontend/src/components/CustomInput/index.tsx
+++ frontend/src/components/CustomInput/index.tsx
@@ -7,56 +7,56 @@ import styles from "./style.module.css";
 const { TextArea } = Input;
 
 export interface InputProps extends AntInputProps {
-    placeholder?: string;
-    size?: SizeType;
-    backgroundColor?: string;
-    bordered?: boolean;
-    className?: string;
-    width?: string;
-    multiline?: boolean;
-    rows?: number;
-    containerStyle?: CSSProperties;
-    onMultilineChange?: any;
+	placeholder?: string;
+	size?: SizeType;
+	backgroundColor?: string;
+	bordered?: boolean;
+	className?: string;
+	width?: string;
+	multiline?: boolean;
+	rows?: number;
+	containerStyle?: CSSProperties;
+	onMultilineChange?: any;
 }
 
 export const CustomInput: React.FC<InputProps> = ({
-    placeholder = "",
-    size = "large",
-    backgroundColor = "rgba(0,0,0,0)",
-    className = "",
-    bordered = false,
-    width = "100%",
-    style,
-    containerStyle,
-    rows = 2,
-    multiline = false,
-    onMultilineChange,
-    ...props
+	placeholder = "",
+	size = "large",
+	backgroundColor = "rgba(0,0,0,0)",
+	className = "",
+	bordered = false,
+	width = "100%",
+	style,
+	containerStyle,
+	rows = 2,
+	multiline = false,
+	onMultilineChange,
+	...props
 }) => {
-    return (
-        <div className={styles.inputContainer} style={containerStyle}>
-            {multiline ? (
-                <TextArea
-                    autoSize={{ minRows: rows, maxRows: 5 }}
-                    rows={5}
-                    placeholder={placeholder}
-                    className={classnames(styles.input, className)}
-                    bordered={bordered}
-                    style={{ backgroundColor, width, ...style }}
-                    defaultValue={props.defaultValue}
-                    onChange={onMultilineChange}
-                />
-            ) : (
-                <Input
-                    placeholder={placeholder}
-                    className={classnames(styles.input, className)}
-                    size={size}
-                    bordered={bordered}
-                    style={{ backgroundColor, width, ...style }}
-                    // eslint-disable-next-line react/jsx-props-no-spreading
-                    {...props}
-                />
-            )}
-        </div>
-    );
+	return (
+		<div className={styles.inputContainer} style={containerStyle}>
+			{multiline ? (
+				<TextArea
+					autoSize={{ minRows: rows, maxRows: 5 }}
+					rows={5}
+					placeholder={placeholder}
+					className={classnames(styles.input, className)}
+					bordered={bordered}
+					style={{ backgroundColor, width, ...style }}
+					defaultValue={props.defaultValue}
+					onChange={onMultilineChange}
+				/>
+			) : (
+				<Input
+					placeholder={placeholder}
+					className={classnames(styles.input, className)}
+					size={size}
+					bordered={bordered}
+					style={{ backgroundColor, width, ...style }}
+					// eslint-disable-next-line react/jsx-props-no-spreading
+					{...props}
+				/>
+			)}
+		</div>
+	);
 };
--- frontend/src/stories/CustomInput.stories.tsx
+++ frontend/src/stories/CustomInput.stories.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import "../App.less";
 import { Story, Meta } from "@storybook/react";
 
 import { CustomInput, InputProps } from "../components/CustomInput";
 
 export default {
-    title: "Shared/CustomInput",
-    component: CustomInput,
+	title: "Shared/CustomInput",
+	component: CustomInput,
 } as Meta;
 
 const Template: Story<InputProps> = (args) => <CustomInput {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-    placeholder: "Sample Input",
+	placeholder: "Sample Input",
 };

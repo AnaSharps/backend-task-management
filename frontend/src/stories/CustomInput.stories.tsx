6ef62f524be9f4ee4f901779765be3a1f94df422
Prashansa Dadu <65507065+AnaSharps@users.noreply.github.com>
2021-07-20 14:03:42 +0530

    Component: Add Input

---
+++ frontend/src/stories/CustomInput.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import { CustomInput, InputProps } from "../components/CustomInput";
+
+export default {
+    title: "Shared/CustomInput",
+    component: CustomInput,
+} as Meta;
+
+const Template: Story<InputProps> = (args) => <CustomInput {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    placeholder: "Sample Input",
+};

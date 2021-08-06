2964fd50eb69e9ed7e495692ae4d134690a7378b
Prashansa Dadu <65507065+AnaSharps@users.noreply.github.com>
2021-08-06 19:50:06 +0530

    Refactor Repetetive Code

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,6 +14,24 @@ use Illuminate\Support\Facades\DB;
 
 class TaskController extends AuthController
 {
+    protected $user;
+    protected $todaysDate;
+    protected $tomorrowsDate;
+    protected $nowTime;
+
+    public function __construct(Request $request)
+    {
+        $token = $request->cookie('token');
+        $payload = (new GenerateJWT)->decodejwt($token);
+
+        if (gettype($payload) !== 'array') return response("Expired token", 403);
+
+        $this->user = strtolower($payload['sub']);
+        $this->todaysDate = date("Y-m-d", time());
+        $this->tomorrowsDate = date("Y-m-d", time() + 60 * 60 * 24);
+        $this->nowTime = date("Y-m-d h:i:s", time());
+    }
+
     public function getTasks(Request $request)
     {
         $this->validate($request, [
@@ -57,34 +75,22 @@ class TaskController extends AuthController
             'status' => 'required|string',
         ]);
 
-        // try {
         $id = $request->id;
         $status = $request->status;
-        $token = $request->cookie('token');
-        $payload = (new GenerateJWT)->decodejwt($token);
 
         $task = Task::findorFail($id);
-        if (gettype($payload) === 'array') {
-            $user = strtolower($payload['sub']);
-            // if (!$task) return response("no such task exists", 400);
-
-            if ($task->assignee !== $user && $task->assignor !== $user) {
-                return response("Unauthorised Request", 403);
-            }
-
-            $task->status = $status;
-            if ($task->save()) {
-                $subject = "Task Status Updated";
-                $view = "emails.taskStatusUpdated";
-                Mail::to([$task->assignee, $task->assignor])->send(new Email("", $subject, $view));
-                return response()->json(['status' => "Task created Successfully", 'task' => $task]);
-            }
-            return response("some error occured", 500);
+        if ($task->assignee !== $this->user && $task->assignor !== $this->user) {
+            return response("Unauthorised Request", 403);
         }
-        return response("Expired token", 403);
-        // } catch (Exception $e) {
-        //     return response($e->getMessage(), 500);
-        // }
+
+        $task->status = $status;
+        if ($task->save()) {
+            $subject = "Task Status Updated";
+            $view = "emails.taskStatusUpdated";
+            dispatch(new SendEmail([$task->assignee, $task->assignor], $subject, $view));
+            return response()->json(['status' => "Task created Successfully", 'task' => $task]);
+        }
+        return response("some error occured", 500);
     }
 
     public function deleteTask(Request $request)
@@ -94,35 +100,24 @@ class TaskController extends AuthController
         ]);
 
         $id = $request->id;
-        $token = $request->cookie('token');
-        // $token = $request->bearerToken('token');
-        $payload = (new GenerateJWT)->decodejwt($token);
 
-        // try {
         $task = Task::findorFail($id);
-        // if (!$task) return response("No such task exists", 404);
 
-        if (gettype($payload) === 'array') {
-            if ($task->assignor !== strtolower($payload['sub'])) {
-                return response("Unauthorised request", 403);
-            }
-            if ($task->delete()) {
-                $subject = "Task Deleted";
-                $view = "emails.deletedTask";
-                Mail::to([$task->assignee, $task->assignor])->send(new Email("", $subject, $view));
-                return response()->json(['status' => 'Successfully deleted!']);
-            }
-            return response("some error occured!", 500);
+        if ($task->assignor !== $this->user) {
+            return response("Unauthorised request", 403);
         }
-        return response("Expired token", 403);
-        // } catch (Exception $e) {
-        //     return response($e->getMessage(), 500);
-        // }
+
+        if ($task->delete()) {
+            $subject = "Task Deleted";
+            $view = "emails.deletedTask";
+            dispatch(new SendEmail([$task->assignee, $task->assignor], $subject, $view));
+            return response()->json(['status' => 'Successfully deleted!']);
+        }
+        return response("some error occured!", 500);
     }
 
     public function createTask(Request $request)
     {
-        // try {
         $this->validate($request, [
             'name' => 'required|string|max:255',
             'desc' => 'required|string|max:255',
@@ -138,38 +133,29 @@ class TaskController extends AuthController
         if (!$assignedUser) {
             return response("No such user exists", 400);
         }
+        $assignor = $this->user;
 
-        $token = $request->cookie('token');
-        $payload = (new GenerateJWT)->decodejwt($token);
-
-        if (gettype($payload) === "array") {
-            $assignor = strtolower($payload['sub']);
-
-            $task = Task::where('taskName', $name)->where('taskDesc', $desc)->where('assignee', $assignee)->where('assignor', $assignor)->first();
-            if ($task) {
-                return response("Task already exists", 400);
-            }
-
-            $due  = $due + time();
-            $task = new Task();
-            $task->taskName = $name;
-            $task->taskDesc = $desc;
-            $task->status = "pending";
-            $task->dueDate = date("Y-m-d h:i:s", $due);
-            $task->assignee = $assignee;
-            $task->assignor = $assignor;
-            if ($task->save()) {
-                $subject = "New Task Assigned!";
-                $view = "emails.newTaskAssigned";
-                dispatch(new SendEmail([$assignee, $assignor], $subject, $view));
-                // Mail::to([$assignee, $assignor])->send(new Email("", $subject, $view));
-                return response()->json(['status' => "Task created Successfully", 'task' => $task]);
-            }
-            return response("some error occured", 500);
+        $task = Task::where('taskName', $name)->where('taskDesc', $desc)->where('assignee', $assignee)->where('assignor', $assignor)->first();
+        if ($task) {
+            return response("Task already exists", 400);
         }
-        return response("Expired token", 403);
-        // } catch (Exception $e) {
-        //     return response($e->getMessage(), 400);
-        // }
+
+        $due  = $due + time();
+
+        $task = new Task();
+        $task->taskName = $name;
+        $task->taskDesc = $desc;
+        $task->status = "pending";
+        $task->dueDate = date("Y-m-d h:i:s", $due);
+        $task->assignee = $assignee;
+        $task->assignor = $assignor;
+
+        if ($task->save()) {
+            $subject = "New Task Assigned!";
+            $view = "emails.newTaskAssigned";
+            dispatch(new SendEmail([$assignee, $assignor], $subject, $view));
+            return response()->json(['status' => "Task created Successfully", 'task' => $task]);
+        }
+        return response("some error occured", 500);
     }
 }
